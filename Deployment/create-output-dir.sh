876a0ffcd691533b537eb94a7ecddef02b2c2382
Vadim Nazaryants <vnazaryants@griddynamics.com>
2015-09-25 13:45:38 +0300

    Fixed hive querying

--- Deployment/create-output-dir.sh
+++ Deployment/create-output-dir.sh
@@ -8,3 +8,5 @@ hadoop fs -mkdir -p /data/visited-products
 hadoop fs -chmod 777 /data
 hadoop fs -chmod 777 /data/bought-products
 hadoop fs -chmod 777 /data/visited-products
+
+hadoop fs -chmod 777 /user
